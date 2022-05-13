0ab9ddcf87ebcbf6af394630ad49639d70a25b65
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2022-05-13 13:48:39 -0700

    Add public IDs to the left side tables

--- api/sql/001-CreateTables2.sql
+++ api/sql/001-CreateTables2.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS PlantResearch (
-    id SERIAL NOT NULL,
+    id          SERIAL  NOT NULL,
     research_id INTEGER NOT NULL,
-    plant_id INTEGER NOT NULL,
+    plant_id    INTEGER NOT NULL,
 
     PRIMARY KEY (id),
     FOREIGN KEY (research_id) REFERENCES Research(id) DEFERRABLE INITIALLY DEFERRED,
@@ -9,18 +9,20 @@ CREATE TABLE IF NOT EXISTS PlantResearch (
 );
 
 CREATE TABLE IF NOT EXISTS HandlingProtocol (
-    id   SERIAL NOT NULL,
-    info VARCHAR(1000) NOT NULL,
+    id              SERIAL        NOT NULL,
+    protocol_number SERIAL        UNIQUE NOT NULL,
+    info            VARCHAR(1000) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS PlantIllness (
-    id          SERIAL       NOT NULL,
-    name        VARCHAR(30)  NOT NULL,
-    description VARCHAR(500) NOT NULL,
-    plant_id    INTEGER,
-    protocol_id INTEGER,
+    id             SERIAL       NOT NULL,
+    illness_number SERIAL       UNIQUE NOT NULL,
+    name           VARCHAR(30)  NOT NULL,
+    description    VARCHAR(500) NOT NULL,
+    plant_id       INTEGER,
+    protocol_id    INTEGER,
 
     PRIMARY KEY (id),
     FOREIGN KEY (plant_id)    REFERENCES Plant(id) DEFERRABLE INITIALLY DEFERRED,
