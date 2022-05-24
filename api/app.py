f49e91617b8fead93dac8d2cb181e66ff33f24d7
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2022-05-24 10:25:33 -0700

    Delete example endpoint

--- api/app.py
+++ api/app.py
@@ -6,28 +6,6 @@ import psycopg2
 app = Flask(__name__)
 app.config['CONNECTION_STRING'] = 'dbname=plantdb user=plantdb password=plantdb'
 
-@app.route('/something')
-def something():
-    # Example of executing database query
-    # https://www.psycopg.org/docs/usage.html#basic-module-usage
-    # https://www.psycopg.org/docs/connection.html#the-connection-class
-    conn = psycopg2.connect(app.config['CONNECTION_STRING'])
-    with conn:
-        with conn.cursor() as cursor:
-            cursor.execute('CREATE TABLE Region (abbr VARCHAR(4) PRIMARY KEY, name VARCHAR(28) UNIQUE NOT NULL);')
-            cursor.execute('INSERT INTO REGION (abbr, name) VALUES (%s, %s);', ('NITL', 'Northern Italy'))
-            cursor.execute('SELECT * FROM Region;')
-            print('Abbreviation: %s\nRegion: %s\n' % cursor.fetchone(), end='')
-            cursor.execute('DROP TABLE Region;')
-    # The transaction is commited after the context exits, here
-
-    # The connection still needs to be closed separately
-    conn.close()
-
-    return {
-        'message': 'ok'
-    }
-
 @app.route('/regions', methods=['GET'])
 def regions():
     """
