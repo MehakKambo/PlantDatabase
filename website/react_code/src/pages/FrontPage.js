a8e0d03628be1ef7d09b1bbf7d97f30108830110
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2022-05-24 10:46:19 -0700

    Fix React framework mount

--- website/react_code/src/pages/FrontPage.js
+++ website/react_code/src/pages/FrontPage.js
@@ -1,12 +1,10 @@
 function FrontPage() {
     return (
         <div>
-            // Search bar for front page 
             <input placeholder="Search" />
         </div>
     );
 }
-
 
 
 export default FrontPage;
