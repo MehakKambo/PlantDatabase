2687457617ea03af6a72a236713c5aa77a9f4d62
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2022-05-24 10:46:19 -0700

    Fix React framework mount

--- website/react_code/src/index.js
+++ website/react_code/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>, document.getElementById('root'));
-
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<BrowserRouter> <App /> </BrowserRouter>);
