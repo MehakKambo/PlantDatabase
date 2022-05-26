eb905e26530deed2485bda5239d6601e396119ba
LucaOnline <82618674+LucaOnline@users.noreply.github.com>
2022-05-26 13:41:03 -0700

    Use parameter routing and an index page

--- website/react_code/src/index.js
+++ website/react_code/src/index.js
@@ -12,7 +12,14 @@ root.render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />}>
-                    <Route path="/plant" element={<PlantPage />} />
+                    <Route path="/plant">
+                        <Route index element={
+                            <main>
+                                <p>No plant provided in the URL.</p>
+                            </main>
+                        } />
+                        <Route path=":scientificName" element={<PlantPage />} />
+                    </Route>
                     <Route path="*" element={<FrontPage />} />
                 </Route>
             </Routes>
