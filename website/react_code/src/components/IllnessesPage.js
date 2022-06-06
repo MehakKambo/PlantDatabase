5df3cedf206a202c3556f5dd2912df3b303f0525
jguilarte21 <jguilarte95@gmail.com>
2022-06-05 19:17:49 -0700

    Symptom Button Added

--- website/react_code/src/components/IllnessesPage.js
+++ website/react_code/src/components/IllnessesPage.js
@@ -1,68 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-
-function SymptomIllnesses({ symptomNumber }) {
-	const [illnesses, setIllnesses] = useState(null);
-	
-	useEffect(() => {
-			// https://plants.ces.ncsu.edu/plants/camellia-sinensis/
-			// This will eventually do an actual request to the API
-			new Promise((resolve) => setTimeout(resolve, 600))
-					.then(() => {
-							setIllnesses([
-									{
-											illnessNumber: 'e3e9e238-d026-4e2a-8d1c-8b2330929f69',
-											name: 'Dieback',
-											description: 'Some description',
-									},
-									{
-											illnessNumber: '5db7858e-dded-4ff9-9c3b-debfd4ff4659',
-											name: 'Cankers',
-											description: 'Some description',
-									},
-									{
-											illnessNumber: '60a96f43-ccf5-4f7a-8e54-05357ecb5388',
-											name: 'Flower blight',
-											description: 'Some description',
-									},
-							]);
-					});
-	}, []);
-
-	if (illnesses == null) {
-			return <div>Loading...</div>
-	}
-
-	return (
-			<ul>
-					{illnesses.map(ill => <li key={symptomNumber + ill.illnessNumber}><a href={`/conditions/${ill.illnessNumber}`}>{ill.name}</a></li>)}
-			</ul>
-	);
-}
-
-
-
-
 export default function IllnessesPage() {
   const { paramName } = useParams();  
-	const [symptoms, setSymptoms] = useState(null);
-	const [selectedSymptom, setSelectedSymptom] = useState('');
-	const { illnessName } = useParams();
 	const [illnesses, setIllnesses] = useState(null);
 
-	useEffect(() => {
-		fetch(`https://plantdb.azurewebsites.net/plants/${paramName}/illness/${illnessName}/symptoms`)
-		.then(res => res.json())
-		.then(data => {
-				setSymptoms(data.symptoms)
-		})
-}, [])
 
-
-const onSymptomClicked = (e) => {
-		setSelectedSymptom(e.target.value);
-};
 
 
     useEffect(() => {
@@ -73,7 +16,7 @@ const onSymptomClicked = (e) => {
         })
     }, [])
 
-    if ((illnesses || symptoms) == null) {
+    if ((illnesses) == null) {
         return <div>Loading...</div>
     }
 
@@ -96,14 +39,11 @@ const onSymptomClicked = (e) => {
 													{ill.description}
 												</td>
 												<td>
-													Possible Symptoms {symptoms.map(sym => (
-                        		<tr key={`symptom-${sym.symptomNumber}`}>
-                            <td><button value={sym.symptomNumber} onClick={onSymptomClicked}>{sym.name}</button></td>
-                            <td>{sym.description}</td>
-                            <td>{selectedSymptom === sym.symptomNumber ? <SymptomIllnesses symptomNumber={sym.symptomNumber} /> : <div />}</td>
-                        </tr>
-                    				))}
+													<button>
+														Symptoms
+													</button>
 												</td>
+
 
 											</tr>
                     ))}
